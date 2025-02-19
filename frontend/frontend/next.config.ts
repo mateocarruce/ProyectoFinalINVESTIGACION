6a0d42cd427ef041fef2b8ee1bbac7a40b3d7b5d
Danyss09 <112229585+Danyss09@users.noreply.github.com>
2025-02-19 16:11:42 -0500

    Revert "Programación lineal"

--- frontend/frontend/next.config.ts
+++ frontend/frontend/next.config.ts
@@ -3,10 +3,5 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-module.exports = {
-  images: {
-    domains: ['127.0.0.1'],  // Agregar este dominio
-  },
-}
 
 export default nextConfig;
